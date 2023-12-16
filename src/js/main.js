3c47f88ae42fad614dbeb4bcf9eaeba57852dd41
Soniamy <marianna.marzullo@gmail.com>
2023-12-16 14:57:50 +0100

    fine esercizio base

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,5 @@
 // carousel image
-const carouselImg = document.querySelector('.carousel-slider');
-
-// button
-const btnUp = document.querySelector('.btn-up');
-const btnDown = document.querySelector('.btn-down');
+const $carouselImg = document.querySelector('.carousel-slider');
 
 // Array di img
 const imgArray = [
@@ -16,13 +12,37 @@ const imgArray = [
 // -----------------------------------------------
 //  CARICO il div "carousel-img"
 // -----------------------------------------------
-
+setInterval(btnDown, 2000);
 for (let i = 0; i < imgArray.length; i++) {
     let itemActive = '';
     if (i == 0) {
         itemActive = 'item-active';
     }
-    carouselImg.innerHTML += ` <div class="item ${itemActive}">
+
+    $carouselImg.innerHTML += ` <div class="item ${itemActive}">
                 <img src=" ${imgArray[i]}" alt="">
             </div>`;
 }
+
+// -----------------------------------------------
+//                     BUTTON
+// -----------------------------------------------
+const $btnUp = document.querySelector('.btn-up');
+const $btnDown = document.querySelector('.btn-down');
+const allItems = document.querySelectorAll('.item');
+let immagineAttualmenteVisibile = 0;
+function btnDown() {
+    if (immagineAttualmenteVisibile < allItems.length - 1) {
+        allItems[immagineAttualmenteVisibile].classList.remove('item-active');
+        immagineAttualmenteVisibile++;
+        allItems[immagineAttualmenteVisibile].classList.add('item-active');
+    }
+}
+$btnUp.addEventListener('click', function () {
+    if (immagineAttualmenteVisibile > 0) {
+        allItems[immagineAttualmenteVisibile].classList.remove('item-active');
+        immagineAttualmenteVisibile--;
+        allItems[immagineAttualmenteVisibile].classList.add('item-active');
+    }
+});
+$btnDown.addEventListener('click', btnDown);
